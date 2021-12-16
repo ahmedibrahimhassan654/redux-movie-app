7551c4246660bed23eeb94431aff42b6511ee419
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-16 19:19:48 +0200

    use axios for api call

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import MovieApi from "../../common/apis/movieApi";
+
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = "Harry";
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(
+        `?i=tt3896198&apikey=ddb706cb&s=${movieText}&type=movie`
+        // `?apiKey=${APIKey}&s=${movieText}&type=movie`
+      ).catch((err) => {
+        console.log(err);
+      });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
