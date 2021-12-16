380c906b8b13bb54d897dd814dd4f43c7b2e1637
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-16 18:26:25 +0200

    style header component and add google font and style app.js component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -15,11 +15,11 @@ function App() {
         <Router>
           <Header></Header>
           <div className="container">
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/movie/:imdbID" element={<MovieDetail />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/movie/:imdbID" component={MovieDetail} />
+              <Route component={PageNotFound} />
+            </Switch>
           </div>
           <Footer />
         </Router>
