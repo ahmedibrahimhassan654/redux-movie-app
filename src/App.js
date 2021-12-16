a1bc21a5a2dbb187ea9c79b19dbd51882c456f21
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-16 17:46:40 +0200

    Add routing to main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from "react";
 
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-     App
-    </div>
+    <>
+      <div className="app">
+        <Router>
+          <Header></Header>
+          <div className="container">
+            <Routes>
+              <Route path="/" exact component={Home} />
+              <Route path="/movie/:imdbID" component={MovieDetail} />
+              <Route component={PageNotFound} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </>
   );
 }
 
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "ddb706cb";
