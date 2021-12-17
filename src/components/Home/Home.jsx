4997da198655d869c8e86eefac778a017cd90d90
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-12-17 13:30:20 +0200

    useDispatch to dispatch action

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import MovieApi from "../../common/apis/movieApi";
-
-
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const movieText = "Harry";
     const fetchMovies = async () => {
       const response = await MovieApi.get(
         `?i=tt3896198&apikey=ddb706cb&s=${movieText}&type=movie`
@@ -13,10 +14,11 @@ const Home = () => {
       ).catch((err) => {
         console.log(err);
       });
-      console.log(response);
+      // console.log(response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img"></div>
